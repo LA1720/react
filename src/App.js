3f21452dd57b21ac7b5cc10062406685af0aff1f
ALI AFZAL <layaafzal74@gmail.com>
2021-01-20 13:56:27 +0100

    react start, bootstrap,first page

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { render } from '@testing-library/react';
+
+function App() {
+  return (
+    <div className="App">
+     <div className="container">
+       <h1 className="display-6">Hello </h1>
+     </div>
+    </div>
+  );
+}
+
+export default App;
